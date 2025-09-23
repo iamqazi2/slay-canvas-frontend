bbcff8d2d1a76d305b424f831744d9cb002f512a
Mustafa Hassan <mustafa.hassan@stixor.com>
2025-09-23 23:41:48 +0500

    refactor: restructure next.config.ts for better organization

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+};
 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://34.67.47.217:8000/api/:path*", // backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
